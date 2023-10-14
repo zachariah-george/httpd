bc8a798943985ac3ce0b5328ae1cfc799702c872
zachariah-george <138194466+zachariah-george@users.noreply.github.com>
2023-10-15 00:36:37 +0530

    Update updateVars.py

--- updateVars.py
+++ updateVars.py
@@ -19,12 +19,12 @@ def get_version_number(soup, keyword):
 
 def get_checksum(file_url, keyword):
    try:
-    with requests.Session() as session:
-        response = session.get(file_url, headers=HEADERS, verify=False)
-        text = response.text
-        pattern = re.compile(fr'SHA1-Checksum for: {keyword}:\n([0-9A-Fa-f]+)', re.M)
-        match = pattern.search(text)
-        return match.group(1)
+       with requests.Session() as session:
+           response = session.get(file_url, headers=HEADERS, verify=False)
+           text = response.text
+           pattern = re.compile(fr'SHA1-Checksum for: {keyword}:\n([0-9A-Fa-f]+)', re.M)
+           match = pattern.search(text)
+           return match.group(1)
 
 
 def update_vars_file(httpd_version, httpd_checksum, mod_security_version, mod_log_rotate_version, crs_version):
