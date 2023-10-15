08293fc4f484ffceff4e8446ac5d0e4504a3dae6
zachariah-george <138194466+zachariah-george@users.noreply.github.com>
2023-10-15 18:15:29 +0530

    Update updateVars.py

--- updateVars.py
+++ updateVars.py
@@ -17,14 +17,15 @@ def get_version_number(soup, keyword):
     version = link['href'].split('-')[1].split('-')[0]
     return version
 
-def get_checksum(file_url, keyword):
-   response = requests.get(file_url, headers=HEADERS, verify=False)
-   text = response.text
-   print(text) 
-   pattern = r"SHA1-Checksum for: .+:\n([0-9A-F]+)"
-   match = re.search(pattern, text)
-   print(match)
-   return match.group(1)
+def calculate_checksum(file_path):
+    sha256_hash = hashlib.sha256()
+    with open(file_path, "rb") as file:
+        while True:
+            data = file.read(65536)  # Read the file in 64KB chunks
+            if not data:
+                break
+            sha256_hash.update(data)
+    return sha256_hash.hexdigest()
 
 
 def update_vars_file(httpd_version, httpd_checksum, mod_security_version, mod_log_rotate_version, crs_version):
@@ -54,9 +55,16 @@ try:
         vs_version = httpd_link['href'].split('-')[-1].split('.')[0][2:]
 
         httpd_file_name = f"httpd-{httpd_version}-win64-{vs_version}.zip"
-        httpd_checksum_link = f'https://www.apachelounge.com/download/{vs_version}/binaries/{httpd_file_name}.txt'
-        httpd_checksum = get_checksum('https://www.apachelounge.com/download/VS17/binaries/httpd-2.4.57-win64-VS17.zip.txt','httpd-2.4.57-win64-VS17.zip')
+        httpd_file_url =f'https://www.apachelounge.com/download/{vs_version}/binaries/{httpd_file_name}'
+        response = session.get(httpd_file_url, headers=HEADERS, verify=False)
+        response.raise_for_status()
 
+        httpd_file_path = f"./{httpd_file_name}"
+        with open(httpd_file_path, 'wb') as file:
+            file.write(response.content)
+        httpd_checksum = calculate_checksum(httpd_file_path)
+        
+        print(f"HTTPD Checksum: {httpd_checksum}")
         print(f"Apache HTTP Server version: {httpd_version}")
         print(f"ModSecurity version: {mod_security_version}")
         print(f"ModLogRotate version: {mod_log_rotate_version}")
